fc7f6bf23be738b9e365c737d5d78f5a310ebe4e
senup <460705520@qq.com>
2020-03-23 08:38:54 +0800

    终于修好swagger2的bug

--- src/main/java/com/wxapp/video/common/WebMvcConfig.java
+++ src/main/java/com/wxapp/video/common/WebMvcConfig.java
@@ -10,11 +10,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Override//配置图片映射的时候会导致swagger被同时影响到
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**")
-                .addResourceLocations("file:D:/wxapp/")
-		.addResourceLocations("classpath:/**")//swagger居然404了，可能拦截次序会影响，后面再做笔记吧
-				.addResourceLocations("classpath:/resources/")//解决swagger静态资源被拦截的问题
-				.addResourceLocations("classpath:/static/")
-				.addResourceLocations("classpath:/public/");
+                .addResourceLocations("classpath:/META-INF/resources/")//解决swagger静态资源被拦截的问题
+                .addResourceLocations("file:D:/wxapp/");
+
 	}
 	
 //	@Bean(initMethod="init")
